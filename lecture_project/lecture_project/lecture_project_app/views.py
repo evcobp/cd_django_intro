5199cbc194c6dace788a66d2a9cadf655ca278fe
Brandon Plavka <47607463+evcobp@users.noreply.github.com>
2020-06-19 10:14:34 -0500

    Adding Session Code

--- lecture_project/lecture_project/lecture_project_app/views.py
+++ lecture_project/lecture_project/lecture_project_app/views.py
@@ -1,5 +1,17 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello World")
+    # first time user experience
+    if 'username' not in request.session:
+        request.session['username'] = 'New User'
+        
+    context = {
+        'username': request.session['username']
+    }
+    return render(request, 'index.html')
+
+def create(request):
+    print(request.POST['username'])
+    request.session['username'] = request.POST['username']
+    return redirect ('/')
