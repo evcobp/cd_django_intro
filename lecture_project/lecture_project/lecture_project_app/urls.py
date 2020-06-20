b4509efa5951885e8cdfbfa5da8ffe4e368c1aeb
Brandon Plavka <47607463+evcobp@users.noreply.github.com>
2020-06-20 08:45:09 -0500

    Added game.html template

--- lecture_project/lecture_project/lecture_project_app/urls.py
+++ lecture_project/lecture_project/lecture_project_app/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index),
-    path('create', views.create)
+    path('create', views.create),
+    path('game', views.game),
+    path('click', views.click)
 ]
--- lecture_project/lecture_project/lecture_project_app/views.py
+++ lecture_project/lecture_project/lecture_project_app/views.py
@@ -5,13 +5,28 @@ def index(request):
     # first time user experience
     if 'username' not in request.session:
         request.session['username'] = 'New User'
-        
+
     context = {
         'username': request.session['username']
     }
-    return render(request, 'index.html')
+    return render(request, 'index.html', context)
 
 def create(request):
     print(request.POST['username'])
     request.session['username'] = request.POST['username']
     return redirect ('/')
+
+def game(request):
+    if 'username' not in request.session:
+        request.session['username'] = 'New User'
+
+    context = {
+        'username': request.session['username'],
+        'times': 0
+    }
+    return render(request, 'game.html', context)
+
+def click(request):
+    # Handle click
+    return redirect('/game')
+    
