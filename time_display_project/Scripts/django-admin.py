8db0abeb8e1efb92dafcb3ef0d3e1b87dad84c9a
Brandon Plavka <47607463+evcobp@users.noreply.github.com>
2020-06-21 18:49:49 -0500

    Second Commit

---
+++ time_display_project/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\brand\codingdojo\python_stack\django\django_intro\time_display_project\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
