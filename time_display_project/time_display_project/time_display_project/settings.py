02aa35e0166c28b9d2deae386e781e99c12e1253
Brandon Plavka <47607463+evcobp@users.noreply.github.com>
2020-06-21 19:30:44 -0500

    Add time_display app and index.html

--- time_display_project/time_display_project/time_display_project/settings.py
+++ time_display_project/time_display_project/time_display_project/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'time_display'
+    'time_display',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
