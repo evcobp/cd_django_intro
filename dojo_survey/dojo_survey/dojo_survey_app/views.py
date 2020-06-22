f8e3fb1d1925c3e5113932d9f40383f691b14212
Brandon Plavka <47607463+evcobp@users.noreply.github.com>
2020-06-21 21:44:45 -0500

    Add functions index and results

--- dojo_survey/dojo_survey/dojo_survey_app/views.py
+++ dojo_survey/dojo_survey/dojo_survey_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    context = {
+        'yourname': request.POST['yourname']
+    }
+    return render(request,'index.html',context)
+
+def results(request):
+    pass
