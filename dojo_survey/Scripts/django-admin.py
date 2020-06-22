d56b481f7249ca87a8518269c1eee65afa07ee1a
Brandon Plavka <47607463+evcobp@users.noreply.github.com>
2020-06-21 21:05:54 -0500

    Initial Commit

---
+++ dojo_survey/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\brand\codingdojo\python_stack\django\django_intro\dojo_survey\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
